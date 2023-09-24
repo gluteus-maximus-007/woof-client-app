5f7c00fcc0c2ddccd88444f1cb3217f1a26b5cf2
igor-srajer <igorsrajer123@gmail.com>
2023-09-24 18:01:45 +0200

    Update amplify environment and configuration

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "clientappamplify86ee688a": {
+    "amplifynextjs8a48fb2c": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
